55bdcfa073425100c46947da152b075bf8aab018
ransayada <ran.sayada@gmail.com>
2021-11-10 13:35:52 +0200

    keep/keep-app: finish in order to free routes

--- js/apps/keep/services/note.service.js
+++ js/apps/keep/services/note.service.js
@@ -9,31 +9,122 @@ export const carService = {
     remove,
     save,
     getEmptyNote,
-    getById,
-    getNextNoteId
+    getById
+    // getNextNoteId
 };
 
-// var gNotes = [
-//     { id: "n101",
-//      type: "note-txt", 
-//      isPinned: true, 
-//      info: { txt: "Fullstack Me Baby!" } 
-//     }, 
-//     { id: "n102", 
-//     type: "note-img", 
-//     info: { url: "http://some-img/me", title: "Bobi and Me" }, 
-//     style: { backgroundColor: "#00d" } 
-//     }, 
-//     { id: "n103", 
-//     type: "note-todos", 
-//     info: { label: "Get my stuff together", todos: [{ txt: "Driving liscence", doneAt: null }, { txt: "Coding power", doneAt: 187111111 }] } 
-//     }
-// ];
+var gNotes = [{
+        id: "n101",
+        type: "note-txt",
+        isPinned: true,
+        info: { txt: "Fullstack Me Baby!" }
+    },
+    {
+        id: "n102",
+        type: "note-img",
+        info: { url: "http://some-img/me", title: "Bobi and Me" },
+        style: { backgroundColor: "#00d" }
+    },
+    {
+        id: "n103",
+        type: "note-todos",
+        info: { label: "Get my stuff together", todos: [{ txt: "Driving liscence", doneAt: null }, { txt: "Coding power", doneAt: 187111111 }] }
+    }
+];
+
+function query(filterBy = {}) {
+    return storageService.query(NOTES_KEY)
+        .then(notes => {
+            return notes;
+        });
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTES_KEY, noteId);
+}
+
+function save(note) {
+    if (note.id) return storageService.put(NOTES_KEY, note);
+    else return storageService.post(NOTES_KEY, note);
+
+}
+
+function getById(noteId) {
+    return storageService.get(NOTES_KEY, noteId);
+}
+
+function getEmptyNote(type) {
+    var op;
+    switch (type) {
+        case "note-txt":
+            op = { txt: "" };
+            break;
+        case "note-img":
+            op = { url: "", title: "" };
+            break;
+        case "note-todos":
+            op = { label: "", todos: [] };
+            break;
+        case "note-video":
+            op = { url: "" };
+            break;
+    }
+    return _createNote(type, false, 'white', op);
+}
 
 
+// iner functions
 
+//style colors table 0-white 1-blue 2-green 3-yellow 4-red 5-purple 6-orange 
+function _addStyle(style) {
+    console.log('you are at _addStyle')
+    var styleNum;
+    switch (style) {
+        case "white":
+            styleNum = 0;
+            break;
+        case "blue":
+            styleNum = 1;
+            break;
+        case "green":
+            styleNum = 2;
+            break;
+        case "yellow":
+            styleNum = 3;
+            break;
+        case "red":
+            styleNum = 4;
+            break;
+        case "purple":
+            styleNum = 5;
+            break;
+        case "orange":
+            styleNum = 6;
+            break;
+    }
+    return styleNum;
+}
 
-// ineer functions
+function _addInfo(info, type) {
+    console.log('you are at _addInfo')
+    var op;
+    switch (type) {
+        case "note-txt":
+            op = { txt: info.txt };
+            break;
+        case "note-img":
+            op = { url: info.url, title: info.title };
+            break;
+        case "note-todos":
+            op = { label: info.label, todos: info.todos };
+            break;
+        case "note-video":
+            op = { url: info.url };
+            break;
+    }
+    return op;
+}
+
 function _createNote(type = 'txt', isPinned = false, style = 'white', info) {
     const note = {
         id: utilService.makeId(),
@@ -44,15 +135,17 @@ function _createNote(type = 'txt', isPinned = false, style = 'white', info) {
     return note;
 }
 
-function _addStyle(style) {
-    console.log('you are at _addStyle')
-}
-
-function _addInfo(info, type) {
-    console.log('you are at _addInfo')
-}
-
 function _createNotes() {
     var notes = utilService.loadFromStorage(NOTES_KEY);
-    if (!notes || notes.length) return notes;
+    if (!notes || notes.length) {
+        notes = [];
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        notes.push(_createNote('txt', false, 'white', { txt: "Fullstack Me Baby!" }));
+        utilService.saveToStorage(NOTES_KEY, notes);
+    }
+    return notes;
 }
