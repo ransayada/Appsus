7834f61d700e6619d22279c9a62386fc432f69e8
oa1996sfor <89141007+oa1996sfor@users.noreply.github.com>
2021-11-13 20:52:00 +0200

    Update home-page.cmp.js

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -3,7 +3,7 @@ export default {
         <section class="flex align-center flex-column" style="font-family: 'Verdana', sans-serif;" >
         <h1>Welcome to Appsus.</h1>
         <p> one place for all of your favorite apps</p>
-        <img src="/img/blowMind.gif">
+        <img src="./img/blowMind.gif">
         </section>
     `
 }
